37db9022c480881d881fa1c3c62616ef47bd5ffb
ShibnathFrontEndDeveloper <shibnath.chakrabarty3@gmail.com>
2024-03-06 20:01:00 +0530

    Create untitled.py

---
+++ untitled.py
@@ -0,0 +1,79 @@
+
+
+
+# Date Types
+
+# number 1
+
+ft = 'String type data'
+print(ft)
+print(type(ft))
+print()
+
+# number 2
+
+ft = 4587558
+print(ft)
+print(type(ft))
+print()
+
+
+# Floating 
+
+ft = 453.5858
+print(ft)
+print(type(ft))
+print()
+
+# Complex
+
+ft = 4j
+print(ft)
+print(type(ft))
+print()
+
+# List
+
+ft = ['1', 2, "Hello"]
+print(ft)
+print(type(ft))
+print()
+
+
+# tuple
+
+ft = (1, '')
+print(ft)
+print(type(ft))
+print()
+
+
+ft = range(100)
+# for x in range:
+#     print(x)
+print(ft)
+print(type(ft))
+print()
+
+ft = {'sangeeta', 'radha', 'shohini'}
+print(ft)
+print(type(ft))
+print()
+
+ft ={'sangeeta': 'radha', 'shohini':'Friend'}
+print(ft)
+print(type(ft))
+print()
+
+ft=frozenset({'sangeeta', 'radha', 'shohini'})
+print(ft)
+print(type(ft))
+print()
+
+ft=b"ft"
+print(ft)
+print(type(ft))
+
+ft=bytearray(10)
+print(ft)
+print(type(ft))
